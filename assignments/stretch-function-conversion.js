7b13002750acb23bba1a9f6b0cb66f606e5f643f
Ronny Alvarado <rsalvarado777@gmail.com>
2019-01-16 20:34:47 -0600

    completed the stretch challenge within the object.js assignment

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -1,6 +1,7 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
 // let myFunction = function () {};
+let myFunction = () => {};
 
 // let anotherFunction = function (param) {
 //   return param;
@@ -12,15 +13,22 @@ const anotherFunction = param => param;
 // };
 // add(1,2);
 const add = (param1, param2) => param1 + param2;
+console.log(add(1,2));
+add(1,2);
 
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+const subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1,2));
+subtract(1,2);
+
 
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
